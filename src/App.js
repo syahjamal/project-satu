dbbb2620930d7e03da9958d48ff909c43ad6c3bd
syahjamal <sulaimansyahjamal@gmail.com>
2020-11-20 16:02:38 +0700

    Merger navbar function to class

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Navbar from './components/Navbar';
+import Navbar from './components/Navbar/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Reports from './pages/Reports';
